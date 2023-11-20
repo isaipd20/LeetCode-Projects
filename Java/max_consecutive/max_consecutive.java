9f49e66acbc16b8b64d16fd3198bd31faf99a707
Armando I. Partida de la O <79038411+isaipd20@users.noreply.github.com>
2023-11-20 01:57:51 -0800

    New problem added

---
+++ Java/max_consecutive/max_consecutive.java
@@ -0,0 +1,28 @@
+package max_consecutive;
+
+public class max_consecutive {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+
+        if(nums.length > 0){
+            for(int i=0; i<nums.length; i++){
+                if(nums[i] == 1){
+                    count++;
+                } else {
+                    if(count >= result){
+                        result = count;
+                    } 
+                    count = 0;
+                }
+                
+                if(i+1 == nums.length && count >= result){
+                    result = count;
+                } 
+                
+            }
+        }
+
+        return result;
+    }
+}
