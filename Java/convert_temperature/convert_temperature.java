35e487ab760e55fb755b0d247b11525c98184868
Armando I. Partida de la O <79038411+isaipd20@users.noreply.github.com>
2023-12-06 20:56:58 -0800

    new problem added

---
+++ Java/convert_temperature/convert_temperature.java
@@ -0,0 +1,12 @@
+package convert_temperature;
+
+public class convert_temperature {
+    public double[] convertTemperature(double celsius) {
+        double[] result = new double[2];
+
+        result[0] = celsius + 273.15;
+        result[1] = celsius * 1.80 + 32.00;
+
+        return result;
+    }
+}
