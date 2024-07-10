de4f3bdf931bfa2836afc330ffbe1429179703b0
Armando I. Partida de la O <79038411+isaipd20@users.noreply.github.com>
2024-07-10 01:08:47 -0700

    new problem added

---
+++ Java/max_product_difference_between_two_pairs/max_product_difference_between_two_pairs.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int maxProductDifference(int[] nums) {
+        int result = 0;
+
+        Arrays.sort(nums);
+
+        result = (nums[nums.length-1] * nums[nums.length-2]) - (nums[0] * nums[1]);
+
+        return result;
+    }
+}
