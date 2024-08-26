463d76fa09473192f70c0bdfe8597c8f199ecb71
Armando I. Partida de la O <79038411+isaipd20@users.noreply.github.com>
2024-08-25 22:52:04 -0700

    new problem added

---
+++ Java/minimum_number_game/minimum_number_game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] numberGame(int[] nums) {
+        int[] result = new int[nums.length];
+        
+        Arrays.sort(nums);
+
+        for(int i = 0; i<nums.length; i=i+2){
+            result[i] = nums[i+1];
+            result[i+1] = nums[i];
+        }
+
+        return result;
+    }
+}
